aa0dcdfa5cf488de815f289b8da7ad61a0a73b35
league <league@imac-12.attlocal.net>
2022-05-04 18:27:22 -0700

    :(

---
+++ src/scheduler/Monday.java
@@ -0,0 +1,16 @@
+package scheduler;
+public enum Monday {
+	Monday, Tuesday, Wednesday,Thursday,Friday;
+	private LinkedList<String> event=new LinkedList<String>();
+	private LinkedList<Integer> eventTime=new LinkedList<Integer>();
+
+	void addEvent(int time, String event) {
+		Node<Integer> node=eventTime.getHead();
+		for(int i =0; i<event.length();i++) {
+		
+			}
+		}
+	}
+	
+
+
--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,5 +1,7 @@
 package scheduler;
 
+import java.util.Scanner;
+
 /*
  * Objective: Create a weekly scheduling application.
  * 
@@ -15,7 +17,7 @@ package scheduler;
  *   day.
  *   
  * - Each day's events should be sorted by chronological order.
- *  
+ *  4
  * - If the user tries to add an event on the same day and time as another event
  *   throw a SchedulingConflictException(created by you) that tells the user
  *   they tried to double book a time slot.
@@ -26,6 +28,46 @@ package scheduler;
 public class Scheduler {
 
     public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+    	LinkedList<Enum> event=new LinkedList<Enum>();
+    	
+    	
+    	
+    	Monday[] events=new Monday[6];
+    	for(int i =0; i<events.length;i++) {
+    		if(i==0) {
+    		events[i]=Monday.Monday;
+    		}
+    		if(i==1) {
+        		events[i]=Monday.Tuesday;
 
+    		}
+    		else	if(i==2) {
+        		events[i]=Monday.Wednesday;
+
+    		}
+    		else	if(i==3) {
+        		events[i]=Monday.Thursday;
+
+    		}
+    		else if(i==4) {
+        		events[i]=Monday.Friday;
+
+    		}
+    	}
+    	
+    	
+    	System.out.print("What do you want to do? Type add to add an event, Type remove to remove an event.");
+        String name = scan.next();
+        if(name.equalsIgnoreCase("Add")) {
+        	System.out.println("Type which day (Weekdays only)");
+        	 String day = scan.next();
+        	 if(day.equalsIgnoreCase("Monday")) {
+        		 System.out.println("What time");
+        		 String dime= scan.next();
+        		 int time= Integer.parseInt(dime);
+        	 }
+        }
+    	
     }
 }
